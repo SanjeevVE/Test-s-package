be6710e3d50c75091689dabae1137671f5e1be56
SanjeevVE <sanjeeveswaramoorthy@gmail.com>
2025-09-08 08:07:15 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Card } from 's-package';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '20px' }}>
+      <Card title='Demo Card'>
+        <p> Hello from my card!</p>
+        <Button onClick={() => alert('Button clicked!')}>Click Me</Button>
+      </Card>
     </div>
   );
 }
